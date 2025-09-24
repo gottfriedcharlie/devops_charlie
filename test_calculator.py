28cac7251940f7243f24990c4bd3031e90f6fbe9
gottfriedcharlie <93043695+gottfriedcharlie@users.noreply.github.com>
2025-09-24 19:16:36 -0400

    fix

--- test_calculator.py
+++ test_calculator.py
@@ -1,13 +1,17 @@
 from calculator import Calculator
 
+
 def test_add():
     calc = Calculator()
     assert calc.add(2, 3) == 5
+
 
 def test_add2():
     calc = Calculator()
     assert calc.add(-2, -3) == -5
 
+
 def test_add3():
     calc = Calculator()
     assert calc.add(6, 7) == 13
+
